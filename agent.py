fedcf8853c1a9eac2597cea37098fd92bc55d39d
break wang <break_1stday@163.com>
2014-04-09 18:07:38 +0800

    Update agent.py
    add rewrite()

--- agent.py
+++ agent.py
@@ -22,19 +22,27 @@ class agent(object):
         print rs,rt  #debug info
         return  rt  
 
-     def rewrite(self,conf_sn='cli1',sv_alias='mymaster1',new_ip='127.0.0.1',new_port=6379):
+    def rewrite(self,conf_sn='cli1',sv_alias='mymaster1',new_ip='127.0.0.1',new_port=6379):
         self.config = self.read_config()        # read config
-        self.twem_config_file=self.config[conf_sn]['twem_config']  # get the twemproxy's config file path
-        self.twem_config=self.read_config(self.twem_config_file)    # read the twemproxy's config file to 'twem_config'
-        for i in range(len(self.twem_config['twem1']['servers'])):
-            if ( self.twem_config['twem1']['servers'][i].split(" ")[1] == sv_alias ):
-                print 'old_addr :',self.twem_config['twem1']['servers'][i].split(" ")[0].split(":")[0],self.twem_config['twem1']['servers'][i].split(" ")[0].split(":")[1]
-                print 'new_addr :',new_ip,new_port
-                self.twem_config['twem1']['servers'][i].split(" ")[0].split(":")[0]=new_ip
-                self.twem_config['twem1']['servers'][i].split(" ")[0].split(":")[1]=new_port
-                print 'changed :',self.twem_config['twem1']['servers'][i]
-            else:
-                print "NOT MATCHING"
+        self.twem_config_file=self.config[conf_sn]['twem_config']
+        self.twem_config=self.read_config(self.twem_config_file)
+        self.twem_servers=self.twem_config['twem1']['servers']
+        print self.twem_servers
+        for i in range(len(self.twem_servers)):
+            if ( self.twem_servers[i].split(" ")[1] == sv_alias ):
+                print 'old_addr :',self.twem_servers[i].split(" ")[0].split(":")
+                weight=self.twem_servers[i].split(" ")[0].split(":")[2]
+                new_addr=new_ip+':'+str(new_port)+':'+weight+' '+sv_alias
+                print 'new_addr :',new_addr 
+                self.twem_servers[i]=new_addr
+                #self.twem_servers[i].split(" ")[0].split(":")[0]=new_ip
+                #self.twem_servers[i].split(" ")[0].split(":")[1]=new_port
+                print 'changed :',self.twem_servers[i]    
+        print self.twem_config
+    
+        with open(self.twem_config_file,'w') as stream:
+            yaml.dump(self.twem_config,stream,allow_unicode=True,default_flow_style=False)
+
 
     def __del__(self):
         pass
