52accc459105b8d5766a77da01bfdbc3427e2fad
break wang <break_1stday@163.com>
2014-04-10 17:24:32 +0800

    Update agent.py

--- agent.py
+++ agent.py
@@ -5,8 +5,8 @@ import json
 import yaml
 import commands
 from redis.sentinel import Sentinel
+from datetime import *
 import time
-import datetime
 
 class agent(object): 
     def __init__(self):
@@ -37,16 +37,14 @@ class agent(object):
     def update_twem_master(self,conf_sn,sv_alias,new_ip,new_port):
         self.load_twem_config(conf_sn)    
         self.twem_servers=self.twem_config['twem1']['servers']
-        print self.twem_servers
+        print "[",datetime.now(),"]","[warning] update master:",conf_sn,sv_alias
         for i in range(len(self.twem_servers)):
             if ( self.twem_servers[i].split(" ")[1] == sv_alias ):
-                print 'old_addr :',self.twem_servers[i].split(" ")[0].split(":")
+                print "[",datetime.now(),"]","old_master :",self.twem_servers[i].split(" ")[0].split(":")
                 weight=self.twem_servers[i].split(" ")[0].split(":")[2]
                 new_addr=new_ip+':'+str(new_port)+':'+weight+' '+sv_alias
-                print 'new_addr :',new_addr 
+                print "[",datetime.now(),"]","new_master",new_addr 
                 self.twem_servers[i]=new_addr
-                print 'changed :',self.twem_servers[i]    
-        print self.twem_config
     
         with open(self.twem_config_file,'w') as stream:
             yaml.dump(self.twem_config,stream,allow_unicode=True,default_flow_style=False)
@@ -60,12 +58,13 @@ class agent(object):
         for i in range(len(self.twem_servers)):
             if( self.twem_servers[i].split(" ")[1] == sv_alias ):
                 self.twem_master=self.twem_servers[i].split(" ")[0].split(':')[0]+':'+self.twem_servers[i].split(" ")[0].split(':')[1]
-        print 'twemproxy_master:',self.twem_master
+        #print 'twemproxy_master:',self.twem_master
         return self.twem_master
 
 
     #retun the sentinel_master addr 
     def get_sentinel_master(self,conf_sn,sv_alias):
+        stats=1
         self.load_config()
         self.sentinel_host=self.config[conf_sn]['sentinel_host']
         self.sentinel_port=self.config[conf_sn]['sentinel_port']
@@ -73,13 +72,16 @@ class agent(object):
             self.sentinel=Sentinel([(self.sentinel_host,self.sentinel_port)],socket_timeout=1)
         except :
             print "connect err"
-        try:
-            self.sentinel_master=self.sentinel.discover_master(sv_alias)
-        except:
-            print "discovering...."
-        self.sentinel_master=self.sentinel_master[0]+':'+str(self.sentinel_master[1])
 
-        print 'sentinel_master:',self.sentinel_master
+        while stats:
+            try:
+                self.sentinel_master=self.sentinel.discover_master(sv_alias)
+                stats=0
+            except:
+                time.sleep(0.5)
+                print "[",datetime.now(),"]","[erro] get sentinel_master failed..."
+
+        self.sentinel_master=self.sentinel_master[0]+':'+str(self.sentinel_master[1])
         return self.sentinel_master
 
         #self.sentinel_slave=self.sentinel.discover_slaves(sv_alias)
@@ -94,6 +96,8 @@ if __name__ == '__main__':
 
     config=ag.load_config('./conf/agent.yml')
     sn=config.keys() #get the config file session name  ,just like 'cli1','cli2'......
+    print "[",datetime.now(),"]","agent is running..."
+    print "[",datetime.now(),"]","session name form agent config file :",sn
 
     while 1:
         for conf_sn in sn:
@@ -102,16 +106,12 @@ if __name__ == '__main__':
             for s in tw_config['twem1']['servers']:
                 sv.append(s.split(" ")[1])    # get the sv_alias list
             for  sv_alias in sv:
-                print conf_sn,sv_alias
                 twem_master=ag.get_twem_master(conf_sn,sv_alias)
                 sentinel_master=ag.get_sentinel_master(conf_sn,sv_alias)
 
-                if ( twem_master == sentinel_master ):
-                    print "OK"
-                else:
+                if ( twem_master != sentinel_master ):
                     new_ip=sentinel_master.split(':')[0]
                     new_port=sentinel_master.split(':')[1]
                     ag.update_twem_master(conf_sn,sv_alias,new_ip,new_port)
 
         time.sleep(1)
-    
