d7a37cb20bf7bf9dd71051239aeb00a23f6a9f40
break wang <break_1stday@163.com>
2014-04-10 14:05:17 +0800

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,37 +1,41 @@
 #!/usr/bin/env python
+
 import os,sys
 import json
 import yaml
 import commands
 from redis.sentinel import Sentinel
+import time
+import datetime
 
-class agent(object):
-    
+class agent(object): 
     def __init__(self):
         pass
-    
+
     #by default,load the agent cnfig file
     def load_config(self,config_file='./conf/agent.yml'):
         self.config_file=config_file
         with open(self.config_file,'r') as fd: 
             self.config = yaml.load(fd)
         return  self.config
-        
+    
     #load the twemproxy config file by agent_conf_sn we specified 
-    def  load_twem_config(self,agent_conf_sn='cli1'):
+    def  load_twem_config(self,agent_conf_sn):
+         self.config=self.load_config()
          self.twem_config_file=self.config[agent_conf_sn]['twem_config']
          self.twem_config=self.load_config(self.twem_config_file)
          return self.twem_config
-        
+    
     #restart the twemproxy 
     def twem_restart(self,agent_conf_sn):  # conf_sn(config file session name) is 'cli1','cli2' etc. we can regard him as  primary key in sql. 
+        self.load_config()
         cmd=self.config[agent_conf_sn]['twem_cmd']
         rs,rt = commands.getstatusoutput(cmd)  # 'rs' means result, 'rt' mean return
         print rs,rt  #debug info
         return  rt  
 
-    def update_twem_master(self,conf_sn='cli1',sv_alias='mymaster1',new_ip='192.168.13.63',new_port=6380):
-        self.load_twem_config(conf_sn)           
+    def update_twem_master(self,conf_sn,sv_alias,new_ip,new_port):
+        self.load_twem_config(conf_sn)    
         self.twem_servers=self.twem_config['twem1']['servers']
         print self.twem_servers
         for i in range(len(self.twem_servers)):
@@ -43,23 +47,71 @@ class agent(object):
                 self.twem_servers[i]=new_addr
                 print 'changed :',self.twem_servers[i]    
         print self.twem_config
-            
+    
         with open(self.twem_config_file,'w') as stream:
             yaml.dump(self.twem_config,stream,allow_unicode=True,default_flow_style=False)
-    
-    def get_twem_master(self,conf_sn='cli1',sv_alias='mymaster1'):
-        print conf_sn,sv_alias,"master addr"
 
+        self.twem_restart(conf_sn)
+
+    #return the twemproxy's master addr (IP:PORT)
+    def get_twem_master(self,conf_sn='cli1',sv_alias='mymaster1'):
+        self.load_twem_config(conf_sn)
+        self.twem_servers=self.twem_config['twem1']['servers']
+        for i in range(len(self.twem_servers)):
+            if( self.twem_servers[i].split(" ")[1] == sv_alias ):
+                self.twem_master=self.twem_servers[i].split(" ")[0].split(':')[0]+':'+self.twem_servers[i].split(" ")[0].split(':')[1]
+        print 'twemproxy_master:',self.twem_master
+        return self.twem_master
+
+
+    #retun the sentinel_master addr 
     def get_sentinel_master(self,conf_sn,sv_alias):
-        print conf_sn,sv_alias,"master addr"
-        
+        self.load_config()
+        self.sentinel_host=self.config[conf_sn]['sentinel_host']
+        self.sentinel_port=self.config[conf_sn]['sentinel_port']
+        try:
+            self.sentinel=Sentinel([(self.sentinel_host,self.sentinel_port)],socket_timeout=1)
+        except :
+            print "connect err"
+        try:
+            self.sentinel_master=self.sentinel.discover_master(sv_alias)
+        except:
+            print "discovering...."
+        self.sentinel_master=self.sentinel_master[0]+':'+str(self.sentinel_master[1])
+
+        print 'sentinel_master:',self.sentinel_master
+        return self.sentinel_master
+
+        #self.sentinel_slave=self.sentinel.discover_slaves(sv_alias)
+        #print 'sentinel:',self.sentinel_host,self.sentinel_port
+        #print 'sentinel_slaves:',self.sentinel_slave
+
     def __del__(self):
         pass
-    
 
 if __name__ == '__main__':
     ag=agent()
-    ag.load_config()
-    #ag.twem_restart('cli1')
-    ag.update_twem_master('cli1','mymaster1','192.168.1.2',6379)
+
+    config=ag.load_config('./conf/agent.yml')
+    sn=config.keys() #get the config file session name  ,just like 'cli1','cli2'......
+
+    while 1:
+        for conf_sn in sn:
+            tw_config=ag.load_twem_config(conf_sn)
+            sv=[]            # get the 
+            for s in tw_config['twem1']['servers']:
+                sv.append(s.split(" ")[1])    # get the sv_alias list
+            for  sv_alias in sv:
+                print conf_sn,sv_alias
+                twem_master=ag.get_twem_master(conf_sn,sv_alias)
+                sentinel_master=ag.get_sentinel_master(conf_sn,sv_alias)
+
+                if ( twem_master == sentinel_master ):
+                    print "OK"
+                else:
+                    new_ip=sentinel_master.split(':')[0]
+                    new_port=sentinel_master.split(':')[1]
+                    ag.update_twem_master(conf_sn,sv_alias,new_ip,new_port)
+
+        time.sleep(1)
     
