31917de8347466e12e598b37788db51c8b08bf28
break wang <break_1stday@163.com>
2014-05-30 18:11:17 +0800

    add log

--- agent.py
+++ agent.py
@@ -7,6 +7,7 @@ import commands
 from redis.sentinel import Sentinel
 from datetime import *
 import time
+import logging
 
 class agent(object): 
     def __init__(self):
@@ -19,31 +20,43 @@ class agent(object):
             self.config = yaml.load(fd)
         return  self.config
     
+    def logger(self,path):
+    	self.logger=logging.getLogger()
+    	self.logger.setLevel(logging.WARNING)
+    	fh=logging.FileHandler(path)
+    	formatter = logging.Formatter("%(asctime)-15s %(filename)s [%(levelname)-8s] %(message)s")
+    	fh.setFormatter(formatter)
+    	self.logger.addHandler(fh)
+    	return self.logger
+
     #load the twemproxy config file by agent_conf_sn we specified 
-    def  load_twem_config(self,agent_conf_sn):
+    def  load_twem_config(self,conf_sn):
          self.config=self.load_config()
-         self.twem_config_file=self.config[agent_conf_sn]['twem_config']
+         self.twem_config_file=self.config['agents'][conf_sn]['twem_config']
          self.twem_config=self.load_config(self.twem_config_file)
          return self.twem_config
-    
+     
     #restart the twemproxy 
-    def twem_restart(self,agent_conf_sn):  # conf_sn(config file session name) is 'cli1','cli2' etc. we can regard him as  primary key in sql. 
+    def twem_restart(self,conf_sn):  # conf_sn(config file session name) is 'cli1','cli2' etc. we can regard him as  primary key in sql. 
         self.load_config()
-        cmd=self.config[agent_conf_sn]['twem_cmd']
+        cmd=self.config['agents'][conf_sn]['twem_cmd']
         rs,rt = commands.getstatusoutput(cmd)  # 'rs' means result, 'rt' mean return
-        print rs,rt  #debug info
+        self.logger.warning("%s"%rt)
         return  rt  
 
     def update_twem_master(self,conf_sn,sv_alias,new_ip,new_port):
         self.load_twem_config(conf_sn)    
         self.twem_servers=self.twem_config['twem1']['servers']
-        print "[",datetime.now(),"]","[warning] update master:",conf_sn,sv_alias
+        self.logger.warning("NO,%s:%s twem_master != sentinel_master,Begin update twemproxy "%(conf_sn,sv_alias))
+
         for i in range(len(self.twem_servers)):
             if ( self.twem_servers[i].split(" ")[1] == sv_alias ):
-                print "[",datetime.now(),"]","old_master :",self.twem_servers[i].split(" ")[0].split(":")
+                self.logger.warning("twem old master:%s"%self.twem_servers[i].split(" ")[0].split(":"))
+                #print "[",datetime.now(),"]","old_master :",self.twem_servers[i].split(" ")[0].split(":")
                 weight=self.twem_servers[i].split(" ")[0].split(":")[2]
                 new_addr=new_ip+':'+str(new_port)+':'+weight+' '+sv_alias
-                print "[",datetime.now(),"]","new_master",new_addr 
+                self.logger.warning("twem new master:%s"%new_addr)
+	        #print "[",datetime.now(),"]","new_master",new_addr 
                 self.twem_servers[i]=new_addr
     
         with open(self.twem_config_file,'w') as stream:
@@ -52,13 +65,14 @@ class agent(object):
         self.twem_restart(conf_sn)
 
     #return the twemproxy's master addr (IP:PORT)
-    def get_twem_master(self,conf_sn='cli1',sv_alias='mymaster1'):
+    def get_twem_master(self,conf_sn,sv_alias):
         self.load_twem_config(conf_sn)
         self.twem_servers=self.twem_config['twem1']['servers']
         for i in range(len(self.twem_servers)):
             if( self.twem_servers[i].split(" ")[1] == sv_alias ):
                 self.twem_master=self.twem_servers[i].split(" ")[0].split(':')[0]+':'+self.twem_servers[i].split(" ")[0].split(':')[1]
         #print 'twemproxy_master:',self.twem_master
+        self.logger.info("get twemproxy %s:%s master:%s"%(conf_sn,sv_alias,self.twem_master))
         return self.twem_master
 
 
@@ -66,12 +80,12 @@ class agent(object):
     def get_sentinel_master(self,conf_sn,sv_alias):
         stats=1
         self.load_config()
-        self.sentinel_host=self.config[conf_sn]['sentinel_host']
-        self.sentinel_port=self.config[conf_sn]['sentinel_port']
+        self.sentinel_host=self.config['agents'][conf_sn]['sentinel_host']
+        self.sentinel_port=self.config['agents'][conf_sn]['sentinel_port']
         try:
             self.sentinel=Sentinel([(self.sentinel_host,self.sentinel_port)],socket_timeout=1)
         except :
-            print "connect err"
+            self.logger.error('connection to Sentinel %s:%s failed '%(self.sentinel_host,self.sentinel_port))
 
         while stats:
             try:
@@ -79,27 +93,39 @@ class agent(object):
                 stats=0
             except:
                 time.sleep(0.5)
-                print "[",datetime.now(),"]","[erro] get sentinel_master failed..."
-
+                self.logger.error('get %s sentinel %s:%s failed %d times'%(conf_sn,self.sentinel_host,self.sentinel_port,stats))
+                stats = stats + 1
+		while (stats == 5):
+                    stats=0
+		    self.logger.error('retry failed,skip this sentinel %s:%s,please check it'%(self.sentinel_host,self.sentinel_port))
+        
         self.sentinel_master=self.sentinel_master[0]+':'+str(self.sentinel_master[1])
-        return self.sentinel_master
-
-        #self.sentinel_slave=self.sentinel.discover_slaves(sv_alias)
-        #print 'sentinel:',self.sentinel_host,self.sentinel_port
-        #print 'sentinel_slaves:',self.sentinel_slave
-
+        if (len(self.sentinel_master)<7):
+            return False
+            self.logger.error("get sentinel %s,%s master Failed"%(conf_sn,sv_alias))
+        else:
+            self.logger.info("get sentinel %s,%s master:%s"%(conf_sn,sv_alias,self.sentinel_master))
+            return self.sentinel_master
+  
     def __del__(self):
         pass
 
 if __name__ == '__main__':
     ag=agent()
-
+    
+    #get config,logger,session name 
     config=ag.load_config('./conf/agent.yml')
-    sn=config.keys() #get the config file session name  ,just like 'cli1','cli2'......
-    print "[",datetime.now(),"]","agent is running..."
-    print "[",datetime.now(),"]","session name form agent config file :",sn
+    logpath=config['global']['agent_log']
+    delay_loop=config['global']['delay_loop']
+    
+    ag.logger(logpath)
+    ag.logger.info('agent start working....')
+    ag.logger.info('load the agent config: %s'%config)
+    sn=config['agents'].keys()
+    print " please note:\n agent is running,all infomation will output to file:%s"%logpath 
 
-    while 1:
+
+    while True:
         for conf_sn in sn:
             tw_config=ag.load_twem_config(conf_sn)
             sv=[]            # get the 
@@ -109,9 +135,10 @@ if __name__ == '__main__':
                 twem_master=ag.get_twem_master(conf_sn,sv_alias)
                 sentinel_master=ag.get_sentinel_master(conf_sn,sv_alias)
 
-                if ( twem_master != sentinel_master ):
+                if ( twem_master != sentinel_master and sentinel_master != False):
                     new_ip=sentinel_master.split(':')[0]
                     new_port=sentinel_master.split(':')[1]
                     ag.update_twem_master(conf_sn,sv_alias,new_ip,new_port)
-
-        time.sleep(1)
+		elif (sentinel_master != False ):
+		    ag.logger.info("OK  %s:%s twem_master==sentinel_master."%(conf_sn,sv_alias))
+        time.sleep(delay_loop)
